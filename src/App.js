3ad5d37898fdda2f7e6486e3bf743b57d717795b
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-02-23 21:33:22 -0500

    i think updated files to mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Router>
+      <Navigation />
+      <Route exact path="/Home" component={Home}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/about" component={About}/>
+    </Router>
   </div>
 );
 
